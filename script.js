8a2cf3d617986769f8abb4ef43eac31be8734c77
alapaque1 <alapaque@easyorderapp.com>
2024-07-17 17:01:29 +0200

    trying fallback stuff

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-async function getPaymentRedirectionUrl(storefrontId, storefrontStoreId, paymentStatus) {
+async function getPaymentRedirectionUrl(fallbackTo, storefrontId, storefrontStoreId, paymentStatus) {
   const url = import.meta.env.VITE_API_URL;
   try {
     const response = await fetch(`${url}/api/v8/ClickAndCollect/Payment/ReturnUrl/${storefrontId}/${storefrontStoreId}/${paymentStatus}`);
@@ -9,15 +9,20 @@ async function getPaymentRedirectionUrl(storefrontId, storefrontStoreId, payment
 
     const result = await response.json();
 
-    redirect(result.returnUrl)
+    redirect(result.returnUrl, fallbackTo);
   } catch (error) {
-    console.error(error.message);
+    console.error('Error fetching payment redirection URL:', error.message);
   }
 }
 
-function redirect(redirectTo) {
-  // Redirect to the specified URL
-  window.location.href = decodeURIComponent(redirectTo);
+function handleWindowError() {
+  alert('The specified url was not found');
+}
+
+function redirect(redirectTo, fallbackTo) {
+  let windowProxy = window.open(decodeURIComponent(redirectTo), '_self');
+
+  windowProxy.onError = window.open(decodeURIComponent(fallbackTo), '_self')
 }
 
 
@@ -25,23 +30,26 @@ window.onload = function () {
   // Get the parameter from the URL
   const params = new URLSearchParams(window.location.search);
   const redirectTo = params.get('redirectTo');
+  const fallbackTo = params.get('fallbackTo');
   const storeId = params.get('storeId');
   const storefrontId = params.get('storefrontId');
   const paymentStatusType = params.get('status');
 
   const _handleRedirectToParameter = () => {
-    redirect(redirectTo);
-  }
+    redirect(redirectTo, fallbackTo);
+  };
 
   const _handleRedirectAfterPayment = () => {
-    void getPaymentRedirectionUrl(storeId, storefrontId, paymentStatusType)
-  }
+    void getPaymentRedirectionUrl(fallbackTo, storeId, storefrontId, paymentStatusType);
+  };
 
   if (redirectTo) {
     _handleRedirectToParameter();
   } else if (storeId && storefrontId && paymentStatusType) {
-    _handleRedirectAfterPayment()
+    _handleRedirectAfterPayment();
   } else {
     console.log('No parameter matching any case provided');
   }
 };
+
+window.onerror = handleWindowError
